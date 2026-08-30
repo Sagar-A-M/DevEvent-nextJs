5caf9713632eb4d093e2a3d0c102ccf0ba55ed9a
Sagar-A-M <hello7nischit@gmail.com>
2026-08-31 01:45:25 +0530

    resolved cache error issue

--- lib/actions/event.actions.ts
+++ lib/actions/event.actions.ts
@@ -1,26 +1,31 @@
-'use server';
-
 import connectToDatabase from "../mongodb";
 import { Event } from "@/database/event.model";
+import { cacheLife, cacheTag } from "next/cache";
+
+const fetchSimilarEventsFromDb = async (slug: string) => {
+    'use cache';
+    cacheLife('hours');
+    cacheTag('similar-events');
+
+    await connectToDatabase();
+
+    const event = await Event.findOne({ slug }).lean();
+
+    if (!event) {
+        return [];
+    }
+
+    const similarEvents = await Event.find({
+        _id: { $ne: event._id },
+        tags: { $in: event.tags },
+    }).lean();
+
+    return JSON.parse(JSON.stringify(similarEvents));
+};
 
 export const GetSimilarEventsBySlug = async (slug: string) => {
-    
-    try{
-
-        await connectToDatabase();
-
-        const event = await Event.findOne({ slug }).lean();
-
-        if (!event) {
-            return [];
-        }
-
-        const similarEvents = await Event.find({
-            _id: { $ne: event._id },
-            tags: { $in: event.tags },
-        }).lean();
-
-        return JSON.parse(JSON.stringify(similarEvents));
+    try {
+        return await fetchSimilarEventsFromDb(slug);
     } catch {
         return [];
     }
